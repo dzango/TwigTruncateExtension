b6fc8dba61b7987339c4fc8366adb257cf4b38e7
Subin <tsubin99@gmail.com>
2016-02-15 16:58:00 +0545

    added truncate method in TruncateTest.php file

---
+++ tests/TruncateTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dzango\Twig\Extension\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class TruncateTest extends PHPUnit_Framework_TestCase
+{
+    public function testTruncate()
+    {       
+    }
+}
