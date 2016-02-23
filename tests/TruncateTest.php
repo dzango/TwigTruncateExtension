98d84993ce36ccef62fa3091c85e4a40d8d419cd
Subin <tsubin99@gmail.com>
2016-02-23 12:39:28 +0545

    created test cases for TruncateTest.php file

--- tests/TruncateTest.php
+++ tests/TruncateTest.php
@@ -10,22 +10,32 @@ class TruncateTest extends PHPUnit_Framework_TestCase
     /**
      * @dataProvider dataProvider
      */
-    public function testTruncate($text, $length, $expected)
+    public function testTruncate($text, $length, $ending, $exact, $considerHtml, $expected)
     {
         $truncate = new Truncate();
 
         $this->assertEquals(
             $expected,
-            $truncate->truncate($text, $length)
+            $truncate->truncate($text, $length, $ending, $exact, $considerHtml)
         );
     }
 
     public function dataProvider()
     {
         return array(
-            array('text to truncate', 1, 'text...'),
-            array('<p>text to truncate</p>', 2, '<p>text</p>...'),
-            array('text', 4, 'text'),
+            array('text to truncate', 1, '...', false, true, '...'),
+            array('text to truncate', 1, '...', true, true, 't...'),
+            array('text', 4, '...', false, true, 'text'),
+            array('text', 1, '...', false, true, '...'),
+            array('text', 2, '...', true, true, 'te...'),
+            array('<div style="color: red;">Hello world</div>', 5, '...', false, true, '<div style="color: red;">Hello</div>...'),
+            array('<div style="color: red;">Hello world</div>', 8, '...', false, false, '<div...'),
+            array('<div style="color: red;">Hello world</div>', 1, '...', false, false, '...'),
+            array('<div style="color: red;">Hello world</div>', 1, '...', true, false, '<d...'),
+            array('<div style="color: red;">Hello world</div>', 3, '...', true, true, '<div style="color: red;">Hel</div>...'),
+            array('<div>Hi friend</div>', 2, '...', false, true, '<div>Hi</div>...'),
+            array('<div>Hi friend</div>', 2, '...', false, false, '...'),
+            array('<div>Hi friend</div>', 5, '...', true, true, '<div>Hi fr</div>...'),
         );
     }
 }
